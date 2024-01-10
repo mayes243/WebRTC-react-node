4da181edef08d1d8cd7bc5298350c18527c3af45
mayeskhatri243 <mayeskhatri243@gmail.com>
2024-01-10 16:48:50 +0530

    options removed  from io

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -10,16 +10,7 @@ export const useSocket = () => {
 
 export const SocketProvider = (props) => {
   const socket = useMemo(
-    () =>
-      io(window?.location?.hostname || process.env.REACT_APP_BASE_URL, {
-        reconnectionDelay: 1000,
-        reconnection: true,
-        reconnectionAttempts: 10,
-        transports: ["websocket"],
-        agent: false,
-        upgrade: false,
-        rejectUnauthorized: false,
-      }),
+    () => io(window?.location?.hostname || process.env.REACT_APP_BASE_URL),
     []
   );
 
